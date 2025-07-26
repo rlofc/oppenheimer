98020a269f5b6dbcb743a9488b6784c01a2e0d94
Ithai Levi <randomlinesofcode@gmail.com>
2025-07-26 13:50:51 +0100

    Feature: show keyboards-shortcuts help window using ?

--- src/main.rs
+++ src/main.rs
@@ -66,8 +66,9 @@ use list::*;
 
 use ratatui::{
     crossterm::event::{self, Event, KeyCode},
-    layout::{Constraint, Layout, Position, Rect},
-    widgets::Paragraph,
+    layout::{Alignment, Constraint, Direction, Layout, Position, Rect},
+    style::{Color, Style, Stylize},
+    widgets::{Block, Borders, Clear, Padding, Paragraph},
     DefaultTerminal, Frame,
 };
 
@@ -121,6 +122,7 @@ pub enum InputMode {
     EditTitle,
     EditItem,
     Search,
+    Help,
 }
 
 impl App {
@@ -185,6 +187,7 @@ impl App {
                             match key.code {
                                 KeyCode::Char('q') => break Ok(()),
                                 KeyCode::Char('/') => self.search(),
+                                KeyCode::Char('?') => self.help(),
                                 KeyCode::Char('u') => self.undo(),
                                 KeyCode::Char('r') => self.redo(),
                                 KeyCode::Char('o') => self.insert_item_to_current_list(),
@@ -256,6 +259,9 @@ impl App {
                                 ()
                             }
                         }
+                    }
+                    InputMode::Help => {
+                        self.input_mode = InputMode::Normal;
                     }
                 }
             }
@@ -508,6 +514,86 @@ impl App {
             _ => {}
         }
         self.draw_status_line(frame, bottom);
+        if self.input_mode == InputMode::Help {
+            self.draw_help_popup(frame);
+        }
+    }
+
+    fn draw_help_popup(&mut self, frame: &mut Frame) {
+        let area = frame.area();
+        let constraints = Constraint::from_fills([2, 1, 2]);
+
+        let chunks = Layout::default()
+            .direction(Direction::Horizontal)
+            .constraints(constraints)
+            .margin(10)
+            .split(area);
+
+        let block = Block::default()
+            .title("Help")
+            .padding(Padding::new(2, 2, 1, 1))
+            .borders(Borders::ALL)
+            .border_style(Style::default().fg(Color::White).bg(Color::Black));
+        let keybindings = vec![
+            ("o", "Add new item to the current list"),
+            ("d", "Delete selected item"),
+            ("Enter", "Edit current item"),
+            ("Ctrl-o", "Add new list to the current board"),
+            ("Ctrl-d", "Delete the current list"),
+            ("Space", "Toggle current item strikethrough"),
+            ("/", "Search for items"),
+            ("Down or j", "Move down"),
+            ("Up or k", "Move up"),
+            ("Right or l", "Move right"),
+            ("Left or h", "Move left"),
+            ("Ctrl-h", "Move item to previous list"),
+            ("Ctrl-l", "Move item to next list"),
+            ("Ctrl-j", "Deprioritize selected item"),
+            ("Ctrl-k", "Prioritize selected item"),
+            ("Shift-h", "Shuffle list right"),
+            ("Shift-l", "Shuffle list left"),
+            ("Tab", "Navigate to the item child-board"),
+            ("Esc", "Go back to the parent-board"),
+            ("u", "Undo action"),
+            ("r", "Redo action"),
+            ("q", "Quit"),
+        ];
+        let mut items: Vec<ratatui::text::Line> = keybindings
+            .iter()
+            .map(|(key, action)| {
+                ratatui::text::Line::from(vec![
+                    ratatui::text::Span::raw(*key).bold().yellow(),
+                    ratatui::text::Span::raw(" ".repeat(14 - key.len())),
+                    ratatui::text::Span::styled(*action, Style::new()),
+                ])
+            })
+            .collect::<Vec<ratatui::text::Line>>();
+        items.insert(
+            0,
+            ratatui::text::Line::from(vec![
+                ratatui::text::Span::raw("key").bold().underlined(),
+                ratatui::text::Span::raw("           "),
+                ratatui::text::Span::raw("Action").bold().underlined(),
+            ]),
+        );
+
+        let content = Paragraph::new(items)
+            .style(Style::default().fg(Color::White).bg(Color::Black))
+            .block(block);
+
+        let footer = Paragraph::new("Press any key to close").dark_gray();
+
+        frame.render_widget(Clear, chunks[1]);
+        frame.render_widget(content, chunks[1]);
+        frame.render_widget(
+            footer.alignment(Alignment::Center),
+            Rect::new(
+                chunks[1].x,
+                chunks[1].y + chunks[1].height - 2,
+                chunks[1].width,
+                1,
+            ),
+        );
     }
 
     fn save_board(&self) {
@@ -517,5 +603,9 @@ impl App {
     fn search(&mut self) {
         self.search.clear();
         self.input_mode = InputMode::Search;
+    }
+
+    fn help(&mut self) {
+        self.input_mode = InputMode::Help;
     }
 }
